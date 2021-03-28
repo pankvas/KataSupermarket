7e8ded54427cb6aee658ce7a3d96e432291652b4
Pankaj Vasantrai <pankajvasantrai@gmail.com>
2021-03-28 17:14:41 +0100

    Add a list of product items.

---
+++ KataSupermarketClassLibrary/Data/Items.cs
@@ -0,0 +1,22 @@
+﻿using KataSupermarketClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataSupermarketClassLibrary.Data {
+    public static class Items 
+    {
+        public static List<Item> ProdItems() 
+        {
+            var items = new List<Item>
+            {
+                new Item { Id = 1, Sku = "A", UnitPrice = 10 },
+                new Item { Id = 2, Sku = "B", UnitPrice = 15 },
+                new Item { Id = 3, Sku = "C", UnitPrice = 40 },
+                new Item { Id = 4, Sku = "D", UnitPrice = 55 }
+            };
+
+            return items;
+        }
+    }
+}
