db97bb93471d92acbe0d3578eee8e616297f1ef8
Pankaj Vasantrai <pankajvasantrai@gmail.com>
2021-03-28 17:33:05 +0100

    Display data from list in console app

---
+++ KataSupermarket.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using KataSupermarketClassLibrary.Models;
+using KataSupermarketClassLibrary.Data;
+using System;
+using System.Collections.Generic;
+
+namespace KataSupermarket.ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Item> _items = Items.ProdItems();
+
+            Console.WriteLine("Shopping List\n");
+
+            Console.WriteLine(" |----------------------------------------------------------------------|\n");
+            Console.WriteLine(" | Item SKU  | Unit Price     | Promotions                              |\n");
+            Console.WriteLine(" |----------------------------------------------------------------------|\n");
+            
+            foreach (var i in _items) {
+                Console.WriteLine(String.Format(" | {0}         | {1}             | {2}                                        |",                                                   
+                    i.Sku, i.UnitPrice, i.Promotion));
+            }
+            Console.WriteLine(" |----------------------------------------------------------------------|\n");
+            Console.WriteLine("\n");
+        }
+    }
+}
